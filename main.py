04b55c1f5de3595816db06534497ac8e9235013c
Zaki <zakiullah132@gmail.com>
2025-12-08 17:41:20 +0100

    main_server have been created

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-
 import pandas as pd
+from sqlalchemy import create_engine
+import urllib.parse
 
+# Load CSV
 df = pd.read_csv("datasets/characters.csv")
-print(df.head())
+
+# Encode password
+password = urllib.parse.quote_plus("Aslam@2023")  # encode special characters
+
+# Connect to PostgreSQL
+engine = create_engine(f"postgresql+psycopg2://postgres:{password}@localhost:5432/Datasets")
+
+# Insert into table
+df.to_sql('characters', engine, if_exists='replace', index=False)
