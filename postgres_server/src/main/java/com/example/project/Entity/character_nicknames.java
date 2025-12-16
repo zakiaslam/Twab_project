f1bea63e6ed4cb5ca49850364fbf7c1c2bea979b
Zaki <zakiullah132@gmail.com>
2025-12-16 12:14:17 +0100

    server for the postgres have been created and connected to the main server

---
+++ postgres_server/src/main/java/com/example/project/Entity/character_nicknames.java
@@ -0,0 +1,4 @@
+package com.example.project.Entity;
+
+public class character_nicknames {
+}
---
+++ postgres_server/src/main/java/com/example/project/Repository/character_nicknamesRepository.java
@@ -0,0 +1,4 @@
+package com.example.project.Repository;
+
+public class character_nicknamesRepository {
+}
