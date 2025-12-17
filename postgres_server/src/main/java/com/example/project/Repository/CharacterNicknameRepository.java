7e19bbc995dcc0eccd9f7b2c500917c0af0a975a
Zaki <zakiullah132@gmail.com>
2025-12-17 13:04:29 +0100

    logic for the combining of the two table have added

--- postgres_server/src/main/java/com/example/project/Repository/CharacterNicknameRepository.java
+++ postgres_server/src/main/java/com/example/project/Repository/CharacterNicknameRepository.java
@@ -1,4 +1,22 @@
 package com.example.project.Repository;
 
-public interface CharacterNicknameRepository {
+import com.example.project.Entity.CharacterNickname;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Repository
+//public interface CharacterNicknameRepository extends JpaRepository<CharacterNickname, Long> {
+//    Optional<CharacterNickname> findByCharacterId(Long characterId);
+//}
+
+public interface CharacterNicknameRepository extends JpaRepository<CharacterNickname, Long> {
+
+    @Query("SELECT cn FROM CharacterNickname cn LEFT JOIN FETCH cn.character")
+    List<CharacterNickname> findAllWithCharacter();
 }
+
+
