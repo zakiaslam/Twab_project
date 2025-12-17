54e8da1e705300cb3a2ea4b850911e88132d27c8
Zaki <zakiullah132@gmail.com>
2025-12-17 13:14:26 +0100

    Entity class for Character have been created

---
+++ postgres_server/src/main/java/com/example/project/Entity/Characters.java
@@ -0,0 +1,83 @@
+package com.example.project.Entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Characters {
+
+    @Id
+    @Column(name = "character_mal_id")
+    private Long characterId;
+
+    private String url;
+
+    private String name;
+    @Column(name = "name_kanji")
+    private String nameKanji;
+
+
+    private String image;
+
+    private Long favourites;
+    @Column(columnDefinition = "TEXT")
+    private String about;
+
+    public Long getCharacterId() {
+        return characterId;
+    }
+
+    public void setCharacterId(Long characterId) {
+        this.characterId = characterId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameKanji() {
+        return nameKanji;
+    }
+
+    public void setNameKanji(String nameKanji) {
+        this.nameKanji = nameKanji;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Long getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(Long favourites) {
+        this.favourites = favourites;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+}
