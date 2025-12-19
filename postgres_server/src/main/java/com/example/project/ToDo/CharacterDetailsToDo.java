466f8d41656b1939e6babe91c522c0e569439458
Zaki <zakiullah132@gmail.com>
2025-12-19 17:30:01 +0100

    CharacterDetailsToDo made to display the details of character after clicking on his name.

---
+++ postgres_server/src/main/java/com/example/project/ToDo/CharacterDetailsToDo.java
@@ -0,0 +1,292 @@
+package com.example.project.ToDo;
+
+public class CharacterDetailsToDo {
+
+    private String title;
+    private String titleJapanese;
+    private String url;
+    private String imageUrl;
+    private String type;
+    private Double score;
+    private String status;
+    private Double scoredBy;
+    private String startDate;
+    private String endDate;
+    private String synopsis;
+    private Double rank;
+    private Long popularity;
+    private Long members;
+    private Long favorites;
+    private String genres;
+    private String studios;
+    private String themes;
+    private String demographics;
+    private String source;
+    private String rating;
+    private Double episodes;
+    private String season;
+    private Double year;
+    private String producers;
+    private String explicitGenres;
+    private String licensors;
+    private String streaming;
+
+    public CharacterDetailsToDo(String title, String titleJapanese, String url, String imageUrl, String type, Double score, String status, Double scoredBy, String startDate, String endDate, String synopsis, Double rank, Long popularity, Long members, Long favorites, String genres, String studios, String themes, String demographics, String source, String rating, Double episodes, String season, Double year, String producers, String explicitGenres, String licensors, String streaming) {
+
+        this.title = title;
+        this.titleJapanese = titleJapanese;
+        this.url = url;
+        this.imageUrl = imageUrl;
+        this.type = type;
+        this.score = score;
+        this.status = status;
+        this.scoredBy = scoredBy;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.synopsis = synopsis;
+        this.rank = rank;
+        this.popularity = popularity;
+        this.members = members;
+        this.favorites = favorites;
+        this.genres = genres;
+        this.studios = studios;
+        this.themes = themes;
+        this.demographics = demographics;
+        this.source = source;
+        this.rating = rating;
+        this.episodes = episodes;
+        this.season = season;
+        this.year = year;
+        this.producers = producers;
+        this.explicitGenres = explicitGenres;
+        this.licensors = licensors;
+        this.streaming = streaming;
+
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleJapanese() {
+        return titleJapanese;
+    }
+
+    public void setTitleJapanese(String titleJapanese) {
+        this.titleJapanese = titleJapanese;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getScoredBy() {
+        return scoredBy;
+    }
+
+    public void setScoredBy(Double scoredBy) {
+        this.scoredBy = scoredBy;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Double getRank() {
+        return rank;
+    }
+
+    public void setRank(Double rank) {
+        this.rank = rank;
+    }
+
+    public Long getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Long popularity) {
+        this.popularity = popularity;
+    }
+
+    public Long getMembers() {
+        return members;
+    }
+
+    public void setMembers(Long members) {
+        this.members = members;
+    }
+
+    public Long getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(Long favorites) {
+        this.favorites = favorites;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getStudios() {
+        return studios;
+    }
+
+    public void setStudios(String studios) {
+        this.studios = studios;
+    }
+
+    public String getThemes() {
+        return themes;
+    }
+
+    public void setThemes(String themes) {
+        this.themes = themes;
+    }
+
+    public String getDemographics() {
+        return demographics;
+    }
+
+    public void setDemographics(String demographics) {
+        this.demographics = demographics;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Double getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(Double episodes) {
+        this.episodes = episodes;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public Double getYear() {
+        return year;
+    }
+
+    public void setYear(Double year) {
+        this.year = year;
+    }
+
+    public String getProducers() {
+        return producers;
+    }
+
+    public void setProducers(String producers) {
+        this.producers = producers;
+    }
+
+    public String getExplicitGenres() {
+        return explicitGenres;
+    }
+
+    public void setExplicitGenres(String explicitGenres) {
+        this.explicitGenres = explicitGenres;
+    }
+
+    public String getLicensors() {
+        return licensors;
+    }
+
+    public void setLicensors(String licensors) {
+        this.licensors = licensors;
+    }
+
+    public String getStreaming() {
+        return streaming;
+    }
+
+    public void setStreaming(String streaming) {
+        this.streaming = streaming;
+    }
+}
