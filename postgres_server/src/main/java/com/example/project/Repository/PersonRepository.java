e09602c46e7432d1bafa006ba79afa718d143b9a
Zaki <zakiullah132@gmail.com>
2025-12-21 18:39:47 +0100

    query and service logic added for the person to find the person by name and id.

--- postgres_server/src/main/java/com/example/project/Repository/PersonRepository.java
+++ postgres_server/src/main/java/com/example/project/Repository/PersonRepository.java
@@ -7,9 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<PersonDetails, Long> {
 
-    @Query("SELECT c FROM PersonDetails c")
+    @Query("SELECT p FROM PersonDetails p")
     Page<PersonDetails> findPerson(Pageable pageable);
+
+    @Query("SELECT p FROM PersonDetails p  where  p.personMalId=:id")
+    List<PersonDetails> findPersonById(Long id);
+
+    @Query("SELECT p FROM PersonDetails p  where  p.name=:name")
+    List<PersonDetails> findPersonByName(String name);
 }
--- postgres_server/src/main/java/com/example/project/services/PersonService.java
+++ postgres_server/src/main/java/com/example/project/services/PersonService.java
@@ -1,11 +1,14 @@
 package com.example.project.services;
 
 
+import com.example.project.Entity.character.Characters;
 import com.example.project.Entity.person.PersonDetails;
 import com.example.project.Repository.CharactersRepository;
 import com.example.project.Repository.PersonRepository;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -15,5 +18,11 @@ public class PersonService {
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+    public List<PersonDetails> getPersonById(Long Id) {
+        return personRepository.findPersonById(Id);
+    }
+    public List<PersonDetails> getPersonByName(String name) {
+        return personRepository.findPersonByName(name);
     }
 }
