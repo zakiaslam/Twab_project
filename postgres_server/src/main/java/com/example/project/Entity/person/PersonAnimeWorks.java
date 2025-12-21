98147c2acfee9e49d32936c2dd6300fa8f67053a
Zaki <zakiullah132@gmail.com>
2025-12-21 14:40:27 +0100

    Classes for the person have been created

---
+++ postgres_server/src/main/java/com/example/project/Entity/person/PersonAnimeWorks.java
@@ -0,0 +1,4 @@
+package com.example.project.Entity.person;
+
+public class PersonAnimeWorks {
+}
--- postgres_server/src/main/java/com/example/project/services/PersonService.java
+++ postgres_server/src/main/java/com/example/project/services/PersonService.java
@@ -3,13 +3,9 @@ package com.example.project.services;
 
 import com.example.project.Entity.person.PersonDetails;
 import com.example.project.Repository.PersonRepository;
-import com.example.project.ToDo.PersonToDo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class PersonService {
@@ -20,28 +16,7 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public List<PersonToDo> personService(Pageable pageable) {
-        Page<PersonDetails> persons = personRepository.findAll(pageable);
-        List<PersonToDo> result = new ArrayList<>();
 
-        for (PersonDetails person : persons) {
-            result.add(new PersonToDo(
-                    person.getPersonMalId(),
-                    person.getUrl(),
-                    person.getWebsiteUrl(),
-                    person.getImageUrl(),
-                    person.getName(),
-                    person.getGivenName(),
-                    person.getFamilyName(),
-                    person.getBirthday(),
-                    person.getFavorites(),
-                    person.getRelevantLocation()
-
-            ));
-        }
-
-        return result;
-    }
 
 
 }
