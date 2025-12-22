eeb0e9d69e49a1ae84211ebcb8019b1acea4e8f4
Zaki <zakiullah132@gmail.com>
2025-12-22 16:00:47 +0100

    service and repository have been made for the Anime to load the data from the query

--- postgres_server/src/main/java/com/example/project/Repository/AnimeRepository.java
+++ postgres_server/src/main/java/com/example/project/Repository/AnimeRepository.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AnimeRepository extends JpaRepository<Details, Long> {
-    @Query("SELECT distinct d FROM Details d")
+    @Query("SELECT distinct d FROM Details d order by d.rank")
     Page<Details> findAnime(Pageable pageable);
 //
-//    @Query("SELECT d FROM Details d  where  d.malId=:id")
-//    List<Details> findAnimeById(Long id);
+    @Query("SELECT d FROM Details d  where  d.malId=:id")
+    List<Details> findAnimeById(Long id);
 //
 //    @Query("SELECT d FROM Details d  where  d.title=:title")
 //    List<Details> findAnimeByName(String title);
--- postgres_server/src/main/java/com/example/project/services/AnimeService.java
+++ postgres_server/src/main/java/com/example/project/services/AnimeService.java
@@ -1,8 +1,8 @@
 package com.example.project.services;
 
+
 import com.example.project.Entity.Anime.Details;
-import com.example.project.Entity.person.PersonDetails;
-import com.example.project.Repository.PersonRepository;
+import com.example.project.Repository.AnimeRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +13,22 @@ import java.util.List;
     @Service
     public class AnimeService {
 
-        private final AnimeService animeService;
+        private AnimeRepository animeRepository;
 
-        public AnimeService(@Lazy AnimeService animeService) {
-            this.animeService = animeService;
+
+//        public AnimeService(@Lazy AnimeService animeService) {
+//            this.animeService = animeService;
+//        }
+
+
+        public AnimeService( AnimeRepository animeRepository) {
+
+            this.animeRepository = animeRepository;
         }
 
-//        public List<Details> getAnimeById(Long Id) {
-//            return animeService.findAnimeById(Id);
-//        }
+        public List<Details> getAnimeById(Long Id) {
+            return animeRepository.findAnimeById(Id);
+        }
 //
 //        public List<Details> getAnimeByName(String name) {
 //            return animeService.findAnimeByName(name);
