daa967c8993240032417ef406cfb2d3ae2293d86
Zakiullah Aslam <forzakiullah.aslam@edu.unito.it>
2026-01-01 11:46:24 +0100

    useless code removed from index.js

--- main_server/public/javascripts/index.js
+++ main_server/public/javascripts/index.js
@@ -54,42 +54,6 @@ function character(event){
 
         window.location.href = '/users/profile';
     }
-//
-// // Wait until the DOM is loaded
-// document.addEventListener("DOMContentLoaded", () => {
-//     const searchbtn = document.getElementById("submitbtn");
-//     if (searchbtn) {
-//         searchbtn.onclick = searchbtnfun;
-//     }
-//     function searchbtnfun(event) {
-//         try {
-//             event.preventDefault();
-//             const query = document.getElementById("searchInput").value;
-//             window.location.href = `/character_anime_works?search=${encodeURIComponent(query)}`;
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
-// });
-// function character_nicknames(event){
-//     event.preventDefault();
-//     console.log("character_nicknames");
-//     window.location.href = '/character_nicknames';
-// }
-// document.addEventListener("DOMContentLoaded", () => {
-//     const searchNickname = document.getElementById("submitbtn");
-//     if (searchNickname) {
-//         searchNickname.onclick = Nickname;
-//     }
-//     function Nickname(event) {
-//         try {
-//             event.preventDefault();
-//             const query = document.getElementById("searchInput").value;
-//             window.location.href = `/character_nicknames?search=${encodeURIComponent(query)}`;
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
-// });
+
 }
 
